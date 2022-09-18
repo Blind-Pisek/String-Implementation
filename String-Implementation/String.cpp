088490db2325627012ed78258cb1f4d30aa6ee91
Karol <pisek.x@gmail.com>
2022-09-18 10:53:10 +0200

    Backup

--- String-Implementation/String.cpp
+++ String-Implementation/String.cpp
@@ -9,102 +9,80 @@ String::String() : str_data(nullptr), str_size(0) {}
 String::String( const char* src_data ) 
 {
 	set_length( strlen(src_data) );
-	for( int it = 0 ; it < str_size ; it++ )
+	for( int it = 0; it < str_size ; it++ )
 	{
 		str_data[it] = src_data[it];
 	}
 }
 
-String::String(const String& _str) {
+String::String( const String& _str ) 
+{
 	set_length(_str.str_size);
-	for (int i = 0; i < str_size; i++) {
+	for ( int i = 0; i < str_size; i++ ) 
+	{
 		str_data[i] = _str.str_data[i];
 	}
 }
-String::~String() {
-	if (str_data != NULL)
+
+String::~String() 
+{
+	if ( str_data != NULL )
 		delete[] str_data;
 }
 
-void String::set_length( const size_t new_size ) 
+// indexes start from 0
+void String::add_char( const char _el, const size_t _index ) 
 {
-	str_size = new_size;
-	str_data = new char[new_size+1]();
-}
-
-void String::set_data( const String& _txt )
-{
-	// method doesn't check if size is grater!
-	for ( size_t it = 0; _txt.str_data[it] != '\0'; it++ ) 
+	if ( _index < 0 || _index > str_size )	// Should have used exeption fault
 	{
-		str_data[it] = _txt.str_data[it];
-	}
-}
-
-char String::get( size_t _pos ) const 
-{
-	if (_pos > str_size) {
-		return 0;
-	}
-	return str_data[_pos];
-}
-
-void String::add( const String& _txt ) 
-{
-	size_t old_str_size = str_size;
-	char* old_str_data = str_data;
-	set_length( str_size + _txt.str_size );
-	set_data( old_str_data );
-
-	for ( size_t it = old_str_size; it < str_size; it++ ) 
-	{
-		str_data[it] = _txt.str_data[it - old_str_size];
-	}
-
-	if( old_str_data != NULL )
-		delete[] old_str_data;
-}
-
-
-// indeksowanie od 0
-void String::add_char(const char _el, const size_t _index) {
-	if (_index < 0 || _index > str_size)
-	{
-		std::cout << "Index error!!!" << std::endl;
+		std::cout << "Index error!!!" << std::endl;	
 		return;
 	}
+
 	size_t old_str_size = str_size;
 	char* old_str_data = str_data;
 	set_length(str_size + 1);
 	
-	for (size_t it = 0; it != _index; it++) {
+	for ( size_t it = 0; it != _index; it++ ) 
+	{
 		this->str_data[it] = old_str_data[it];
 	}
+
 	this->str_data[_index] = _el;
-	for (size_t it = _index + 1; it != this->str_size; it++) {
+
+	for ( size_t it = _index + 1; it != this->str_size; it++ ) 
+	{
 		this->str_data[it] = old_str_data[it-1];
 	}
 }
-// indeksowanie od 0
-void String::delete_element(const size_t _index) {
-	if (_index < 0 || _index > str_size) {
+
+// indexes start from 0
+void String::delete_element(const size_t _index) 
+{
+	if ( _index < 0 || _index > str_size ) // TODO exeption fault
+	{
 		std::cout << "Index error!!!" << std::endl;
 		return;
 	}
 
-	for (size_t it = _index; it < str_size-1; it++) {
+	for ( size_t it = _index; it < str_size-1; it++ ) 
+	{
 		str_data[it] = str_data[it + 1];
 	}
 	str_size--;
 	str_data[str_size] = '\0';
 }
-// indeksowanie od 0
-void String::erase(size_t _pos, size_t _len) {
-	if (_pos<0 || _pos>str_size || _len<0 || _len>str_size || _pos+_len>str_size) {
+
+// indexes start from 0
+void String::erase( size_t _pos, size_t _len ) 
+{
+	if ( _pos<0 || _pos>str_size || _len<0 || _len>str_size || _pos+_len>str_size ) 
+	{
 		std::cout << "Index error!!!" << std::endl;
 		return;
 	}
-	for (size_t it = _pos; it < _len + _pos; it++) {
+	for (size_t it = _pos; it < _len + _pos; it++) 
+	{
 		this->delete_element(_pos);
 	}
 }
@@ -168,5 +146,49 @@ String operator+(const String& _str1, const String& _str2) {
 	_string.add(_str2);
 	return _string;
 }
+
+
+
+void String::set_length( const size_t new_size ) 
+{
+	str_size = new_size;
+	str_data = new char[new_size+1]();
+}
+
+void String::set_data( const String& _txt )
+{
+	// method doesn't check if size is grater!
+	for ( size_t it = 0; _txt.str_data[it] != '\0'; it++ ) 
+	{
+		str_data[it] = _txt.str_data[it];
+	}
+}
+
+char String::get( size_t _pos ) const 
+{
+	if (_pos > str_size) {
+		return 0;
+	}
+	return str_data[_pos];
+}
+
+void String::add( const String& _txt ) 
+{
+	size_t old_str_size = str_size;
+	char* old_str_data = str_data;
+	set_length( str_size + _txt.str_size );
+	set_data( old_str_data );
+
+	for ( size_t it = old_str_size; it < str_size; it++ ) 
+	{
+		str_data[it] = _txt.str_data[it - old_str_size];
+	}
+
+	if( old_str_data != NULL )
+		delete[] old_str_data;
+}
+
+
+
 
 }
--- String-Implementation/String.h
+++ String-Implementation/String.h
@@ -1,12 +1,12 @@
 #ifndef MY_STRING_H 
 #define MY_STRING_H
 
-/* File Name         : tring.h
+/* File Name         : string.h
 * Author             : Karol Pisarski
+* Email				 : pisek.x@gmail.com
 * Version            : v1.2
 * Date               : 17/09/2022
-* Desciption         : This file contains all the functions for the
-*   				   string library
+* Desciption         : This file contains the functions for the string library			   
 ********************************************************************************/
 
 /* includes */
@@ -54,59 +54,50 @@ class It
 /// @brief 
 class String 
 {
+	public:
+		/* Public attributes */
+
 	private:
 		/* Private attributes */
 		char* str_data;		// pointer to string data
 		size_t str_size;	// string size
 
-		/* Private methods */
-		/// @brief Sets string size
-		//	Warning! Method erases data inside object
-		/// @param new_size Size of new string
-		void set_length( const size_t new_size );
-
-		/// @brief Copies data from one string to another
-		/// @param _txt String to be copied from
-		void set_data( const String& _txt );
-
-		/// @brief Gets char from provided position
-		/// @param _pos Position to String
-		/// @return Char or 0 if position is grater than 
-		///			String size
-		char get( size_t _pos ) const;
-
-		/// @brief String concatenate
-		/// @param _txt String to be added
-		void add( const String& _txt );
-
 	public:
-		/* Private attributes */
-
-		/* Private methods */
-		/// @brief Creates empty String with size 0
-		///		   without data
+		/* Public methods */
+		/// @brief Creates empty String with size 0 without data
 		String();
 
-		/// @brief 
-		/// @param s_size 
-		String(const size_t s_size);
+		// /// @brief 
+		// /// @param s_size 
+		// String(const size_t s_size);		// TODO
 
-		/// @brief 
-		/// @param src_data 
-		String(const char* src_data);
+		/// @brief Constructs string with C char array
+		/// @param src_data C char array
+		String(const char* src_data);	
 
-		/// @brief 
-		/// @param _str 
+		/// @brief Copy constructor
+		/// @param _str string to be copied from
 		String(const String& _str);
 
 		~String();
 
-
+		/// @brief Adds char into string on specified position
+		/// @param _el char to be inserted
+		/// @param _index position to be inserted
 		void add_char(const char _el, const size_t _index);
 
+		/// @brief Delets chosen element
+		/// @param _index number of element to be deleted
 		void delete_element(const size_t _index);
 
+		/// @brief 
+		/// @param _pos 
+		/// @param _len 
 		void erase(size_t _pos, size_t _len);
+
+		/// @brief 
+		/// @param _first 
+		/// @param _last 
 		void erase(It<char> _first, It<char> _last);
 	
 		void to_upper();
@@ -127,9 +118,31 @@ class String
 			It<char> it(str_data + str_size);
 			return it;
 		}
+		
 
-};
-}
+
+	private:
+		/* Private methods */
+		/// @brief Sets string size
+		//	Warning! Method erases data inside object
+		/// @param new_size Size of new string
+		void set_length( const size_t new_size );
+
+		/// @brief Copies data from one string to another
+		/// @param _txt String to be copied from
+		void set_data( const String& _txt );
+
+		/// @brief Gets char from provided position
+		/// @param _pos Position to String
+		/// @return Char or 0 if position is grater than String size		
+		char get( size_t _pos ) const;
+
+		/// @brief String concatenate
+		/// @param _txt String to be added
+		void add( const String& _txt );
+
+}; /* Class String */
+} /* namespace kp */
 
 
 #endif /* MY_STRING_H */
